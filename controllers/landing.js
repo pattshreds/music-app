242711077f3140aa36f67eb54e12ef7b1c13cf42
Jasmine Smith <Jas@Jasmines-MacBook-Air-2.local>
2021-01-07 10:30:19 -0700

    details

--- controllers/landing.js
+++ controllers/landing.js
@@ -7,28 +7,4 @@ landing.get('/landing', (req, res) => {
   {currentUser: req.session.currentUser})
 });
 
-// sessions.post('/', (req, res) => {
-//   User.findOne({username: req.body.username}, (err, foundUser) => {
-//     if (err) {
-//       console.log(err)
-//       res.send('The db had a problem')
-//     } else if (!foundUser) {
-//       res.send('<a href="/">Can\'t find this user</a>')
-//     } else {
-//       if (bcrypt.compareSync(req.body.password, foundUser.password)) {
-//         req.session.currentUser = foundUser
-//         res.redirect('/')
-//       } else {
-//         res.send('<a href="/"> Password Does not match :( </a>')
-//       }
-//     }
-//   })
-// })
-
-// sessions.delete('/', (req, res) => {
-//   req.session.destroy(() => {
-//     res.redirect('/')
-//   })
-// })
-
 module.exports = landing
