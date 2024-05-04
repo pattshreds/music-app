432d2e2449b44336965567596da1be861eefde0f
Patrick Mcguigan <pattshreds@patricks-mbp.lan>
2024-05-03 19:07:16 -0600

    Begninning the process of building Moodset to be bigger, better, faster, and stronger! Scratched all css, restyling using Materialize. Finished landing page for mobile and desktop sizes. Pretty responsive out of the box! Had to use a few custom css rules in the landingStyles.css file as a way to center the landing gif but not allow it to be 100% width at desktop screen size.

--- server.js
+++ server.js
@@ -9,21 +9,26 @@ const db = mongoose.connection;
 require('dotenv').config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
 });
 
-app.use(fileUpload({
-  useTempFiles: true
-}));
+app.use(
+    fileUpload({
+        useTempFiles: true,
+    })
+);
 
 const PORT = process.env.PORT;
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
-);
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
 
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
@@ -36,11 +41,12 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: false
-})
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
 );
 
 //====================
@@ -51,7 +57,7 @@ const appController = require('./controllers/app.js');
 app.use('/playlist', appController);
 
 app.get('/', (req, res) => {
-  res.redirect('/playlist')
+    res.redirect('/playlist');
 });
 
 const userController = require('./controllers/users.js');
@@ -63,11 +69,10 @@ app.use('/sessions', sessionsController);
 const landingController = require('./controllers/landing.js');
 app.use('/landing', landingController);
 
-
 //====================
 // Listener
 //====================
 
 app.listen(PORT, () => {
-  console.log( 'Listening on port:', PORT)
+    console.log('Listening on port:', PORT);
 });
